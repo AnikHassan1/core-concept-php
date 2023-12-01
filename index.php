e2828fedfcd95ea8b89f41a6f277419d6577a0b8
Anik Hassan1 <ah491606@gmail.com>
2023-12-01 19:24:03 +0600

    core

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// echo (acos(2));
+// echo (acos(-1));
+// echo (acos(0));
+
+#base convert
+// $hex = "E196";
+// echo base_convert($hex,16,8);
+echo(pi())."<br>";#pi 
+echo(min(-1,4,5,65,43,24))."<br>";#minimum
+echo(max(-1,4,5,65,43,24))."<br>";#maximun
+echo(abs(-678))."<br>";#absolute
+echo(sqrt(64))."<br>";#sqrt
+echo(round(.99))."<br>";#Round floating-point number to its nearest integer
+echo(rand(5,10))."<br>";#Random Number 
+echo(ceil(9.90))."<br>";#ceil return up to nearest number
+echo(floor(9.90))."<br>";#floor return down to nearest number
+echo(fmod(20,3))."<br>";# return reminder number
+echo hexdec("a")."<br>";#convert hex to dec
+
+?>
---
+++ ternary.php
@@ -0,0 +1,16 @@
+<?php
+//  $name = "b";
+//  echo $name = (empty($name))?"Enter name":"login";
+?>
+
+<?php
+// $name="anik";
+// if($name == null){
+//     echo " Enter your name";
+// }else{
+//     echo "login";
+// }
+
+#$gender ="male";
+echo $name =$name ?? "Karim";
+?>
